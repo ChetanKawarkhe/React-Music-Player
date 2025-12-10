1cd7dd79e43107a0324abaca4125082efcaaf69b
Chetan <51225903+ChetanKawarkhe@users.noreply.github.com>
2025-12-10 14:41:40 +0530

    Final Push

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,7 +6,7 @@ import { PlayerContext } from '../context/PlayerContext'
 
 const Player = () => {
 
-  const {seekBar, seekBg, playStatus, play, pause,track,time} = useContext(PlayerContext)
+  const {seekBar, seekBg, playStatus, play, pause,track,time,previous, next,seekSong} = useContext(PlayerContext)
 
   return (
     <div className='h-[10%] bg-bblack flex  justify-between items-center text-white px-4'>
@@ -21,21 +21,21 @@ const Player = () => {
       <div className="flex flex-col items-center gap-1 m-auto">
         <div className="flex gap-4">
           <img src={assets.shuffle_icon} alt="" className="w-4 cursor pointer" />
-          <img src={assets.prev_icon} alt="" className="w-4 cursor pointer" />
+          <img onClick={previous} src={assets.prev_icon} alt="" className="w-4 cursor pointer" />
           {
             playStatus ?
             <img onClick={pause} src={assets.pause_icon} alt="" className="w-4 cursor pointer" />
             : <img onClick={play} src={assets.play_icon} alt="" className="w-4 cursor pointer" />
           }
-          <img src={assets.next_icon} alt="" className="w-4 cursor pointer" />
+          <img onClick={next} src={assets.next_icon} alt="" className="w-4 cursor pointer" />
           <img src={assets.loop_icon} alt="" className="w-4 cursor pointer" />
         </div>
         <div className="flex items-center gap-5">
-          <p>{time.currentTime.minutes}:{time.currentTime.second}}</p>
-          <div className='w-[60vw] max-w-[500px] bg-gray-500 rounded-full cursor-pointer'>
-            <hr className='h-1 border-none w-0 bg-green-800 rounded-full' />
+          <p>{time.currentTime.minute}:{time.currentTime.second}</p>
+          <div  onClick={seekSong} ref={seekBg}  className='w-[60vw] max-w-[500px] bg-gray-500 rounded-full cursor-pointer'>
+            <hr ref={seekBar} className='h-1 border-none w-0 bg-green-800 rounded-full' />
           </div>
-          <p>{time.totalTime.minute}:{time.TotalTime.second}</p>
+          <p>{time.totalTime.minute}:{time.totalTime.second}</p>
         </div>
       </div>
       <div className="hidden lg:flex items-center opacity-75 gap-2">
